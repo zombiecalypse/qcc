53dc73e5baf39266e03efc52c90398e628b78045
Aaron Karper <akarper@google.com>
2015-01-22 23:43:04 +0100

    Init commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='qcc',
+      version='0.1',
+      description='Random test to check properties.',
+      author='Aaron Karper',
+      author_email='maergil@gmail.com',
+      url='https://github.com/zombiecalypse/',
+      packages=['qcc'],
+     )
+
